0fd5386acd9dedf483cb8a59f4d52fa4c5e752f9
josenberg <jose.philipe@hotmail.com>
2015-09-23 20:59:42 -0300

    Add Jquery

--- index.js
+++ index.js
@@ -1,21 +1,30 @@
-function convertFromHex(hex) {
-    var hex = hex.toString();//force conversion
-    var str = '';
-    for (var i = 0; i < hex.length; i += 2)
-        str += String.fromCharCode(parseInt(hex.substr(i, 2), 16));
-    return str;
-}
+ $(document).ready(function(){
 
-function convertToHex(str) {
-    var hex = '';
-    for(var i=0;i<str.length;i++) {
-        hex += ''+str.charCodeAt(i).toString(16);
-    }
-    return hex;
-}
+	function convertFromHex(hex) {
+	    var hex = hex.toString();//force conversion
+	    var str = '';
+	    for (var i = 0; i < hex.length; i += 2)
+	        str += String.fromCharCode(parseInt(hex.substr(i, 2), 16));
+	    return str;
+	}
+
+	function convertToHex(str) {
+	    var hex = '';
+	    for(var i=0;i<str.length;i++) {
+	        hex += ''+str.charCodeAt(i).toString(16);
+	    }
+	    return hex;
+	}
 
 
-var input = "Philipe Godoy";
-var hexa_value = convertToHex(input).toString();
-var hexa_colors = hexa_value.match(/.{1,6}/g);
-console.log("hexa_colors ->", hexa_colors);
+	var input = "Philipe Godoy";
+	var hexa_value = convertToHex(input).toString();
+	var hexa_colors = hexa_value.match(/.{1,6}/g);
+	console.log("hexa_colors ->", hexa_colors);
+	/*for (var i = hexa_colors.length - 1; i >= 0; i--) {
+		hexa_colors[i];
+	};*/
+
+
+ });
+
