6f88b6ba8a6e96f9a74e9a920d16c01c948980ec
josenberg <jose.philipe@hotmail.com>
2015-09-23 20:57:34 -0300

    Getting colors into a array

---
+++ README.md
@@ -0,0 +1,5 @@
+#Intro
+
+Todos os digitos que existem no mundo digital são na verdade uma combinação de "zeros e uns", cada numero possue um codigo proprio, mas esse codigo alem de simbolizar um char, ele tambem pode representar um valor decimal, esse valor decimal pode ser convertido para hexadecimal. 
+
+Como todas as cores RGB podem ser representadas por um codigo Hexadecimal, é possivel transformar um texto em uma sequencia de cores, e essa sequencia de cores terá o mesmo valor binario que o texto original.
---
+++ index.js
@@ -0,0 +1,21 @@
+function convertFromHex(hex) {
+    var hex = hex.toString();//force conversion
+    var str = '';
+    for (var i = 0; i < hex.length; i += 2)
+        str += String.fromCharCode(parseInt(hex.substr(i, 2), 16));
+    return str;
+}
+
+function convertToHex(str) {
+    var hex = '';
+    for(var i=0;i<str.length;i++) {
+        hex += ''+str.charCodeAt(i).toString(16);
+    }
+    return hex;
+}
+
+
+var input = "Philipe Godoy";
+var hexa_value = convertToHex(input).toString();
+var hexa_colors = hexa_value.match(/.{1,6}/g);
+console.log("hexa_colors ->", hexa_colors);
