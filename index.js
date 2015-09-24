72f26da7e2c28f4f339ff65de18e8c7b02629db7
josenberg <jose.philipe@hotmail.com>
2015-09-23 21:05:00 -0300

    Add boxers colors

--- index.js
+++ index.js
@@ -21,10 +21,20 @@
 	var hexa_value = convertToHex(input).toString();
 	var hexa_colors = hexa_value.match(/.{1,6}/g);
 	console.log("hexa_colors ->", hexa_colors);
-	/*for (var i = hexa_colors.length - 1; i >= 0; i--) {
-		hexa_colors[i];
-	};*/
 
+
+
+
+	for (var i = hexa_colors.length - 1; i >= 0; i--) {	
+		var currrent_color = hexa_colors[i];
+		if(currrent_color.length === 6){
+			$("#results").append("<div class='box-color' style='background-color:#" +  currrent_color + "'></div>");	
+		}else {
+			$("#results").append("<h1>" + currrent_color + "</h1> <br />");	
+		}
+		
+	};
+	
 
  });
 
